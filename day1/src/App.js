3babd74909de710cc92ded24bf54e44f33a1a829
jiajianhui <jia15176168273@icloud.com>
2024-09-16 17:20:51 +0800

    day1；事件绑定、组件、useState

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 // 测试数据
-const count = 100
+const count = 100;
 function getName() {
-  return 'jian'
+  return "jian";
 }
 const list = [
   { id: 1, name: "vue" },
@@ -12,20 +13,38 @@ const list = [
   { id: 3, name: "hello" },
 ];
 
-const isLogin = true
+const isLogin = true;
 
-const articleType = 1
+const articleType = 1;
 function getArticle() {
   if (articleType === 0) {
-    return <div>无图模式</div>
+    return <div>无图模式</div>;
   } else if (articleType === 1) {
-    return <div>单图模式</div>
+    return <div>单图模式</div>;
   } else {
-    return <div>多图模式</div>
+    return <div>多图模式</div>;
   }
 }
 
+// 将事件对象和自定义参数传入
+function handleClick(name, e) {
+  console.log("hello", name, e);
+}
+
+// 定义组件；首字母大写，内部存放逻辑和视图UI
+const Button = () => {
+  return <button>hello</button>;
+};
+
+
+
 function App() {
+  // useState
+  // 必须在函数组件顶层调用，这是 React Hooks 的规则之一
+  // num为状态变量；setNum为修改状态变量的方法
+  const [num, setNum] = useState(0);
+  const [myName, setmyName] = useState({ name: "jian" });
+
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -64,7 +83,6 @@ function App() {
 
       {/* 2、列表渲染 */}
       <ul>
-        
         {list.map((item) => (
           // map 循环哪个结构就return哪个结构
           // 注意事项：加上一个独一无二的key（字符串或number），名称一般为id
@@ -76,13 +94,43 @@ function App() {
       {/* 3、条件渲染 */}
       <div>
         {/* 1、逻辑与 && */}
-        {isLogin && 'hello'}
+        {isLogin && "hello"}
 
         {/* 2、三元运算 */}
-        {isLogin ? '曹操' : '刘备'}
+        {isLogin ? "曹操" : "刘备"}
 
         {/* 3、复杂条件渲染 */}
         {getArticle()}
+      </div>
+
+      {/* 4、事件绑定 */}
+      <div>
+        <button onClick={(e) => handleClick("jian", e)}>clicke me</button>
+      </div>
+
+      {/* 5、组件 */}
+      {/* 将组件当成标签来写 */}
+      <Button />
+
+      {/* 6、useState */}
+      <div>
+        <button
+          onClick={() => {
+            // 修改普通数据
+            setNum(num + 1);
+
+            // 修改对象数据（数组同理）
+            setmyName({
+              // 使用展开运算符保留对象的其他属性，并更新 `name` 属性
+              ...myName,
+              name: "hui",
+            });
+          }}
+        >
+          {num}
+          {/* 这里渲染对象的属性 */}
+          {myName.name}
+        </button>
       </div>
     </div>
   );
