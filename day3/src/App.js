9f1d0971b65ccfe368ed7b96cac4fa45c011f430
jiajianhui <jia15176168273@icloud.com>
2024-09-29 17:49:17 +0800

    day3；Redux与React-异步状态操作

--- day3/src/App.js
+++ day3/src/App.js
@@ -3,10 +3,18 @@ import { useSelector, useDispatch } from "react-redux";
 
 // react组件中修改store中的数据——借用useDispatch函数，它的作用是生成dispatch函数（用来触发action对象）
 import { decrement, inscrement, addToNum } from "./store/modules/counterStore";
+import { useEffect } from "react";
+import { fetchChannelList } from "./store/modules/channelStore";
 
 function App() {
   const { count } = useSelector((state) => state.counter); //这里的counter对应的是根store中reducer对象中的counter
+  const { channelList } = useSelector((state) => state.channel);
   const dispatch = useDispatch();
+
+  // 使用useEffect触发异步请求获取列表
+  useEffect(() => {
+    dispatch(fetchChannelList())
+  })
 
   return (
     <div className="App">
@@ -15,6 +23,10 @@ function App() {
       <button onClick={() => dispatch(inscrement())}>+</button>
       <button onClick={() => dispatch(addToNum(10))}>+10</button>
       <button onClick={() => dispatch(addToNum(20))}>+20</button>
+
+      <ul>
+        {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   );
 }
--- day3/src/store/index.js
+++ day3/src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // 导入子模块
 import counterReducer from './modules/counterStore'
+import channelReducer from './modules/channelStore'
 
 // 创建根store，组合子模块
 const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        channel: channelReducer
     }
 })
 
