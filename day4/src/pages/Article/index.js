ad7ec2d7e2e7d89a3b807c6dc8b267a550f00729
jiajianhui <jia15176168273@icloud.com>
2024-10-08 17:36:28 +0800

    day4；路由导航跳转

--- day4/src/pages/Article/index.js
+++ day4/src/pages/Article/index.js
@@ -1,5 +1,22 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const Article = () => {
-  return <div>Article</div>;
+  // 生成导航方法
+  const navigate = useNavigate()
+
+  return (
+    <div>
+      <div>Article</div>
+
+      {/* 声明式写法——适合写在模版中 */}
+      <Link to={"/login"}>跳转到Login</Link>
+
+      {/* 编程式写法——适合写在逻辑代码中 */}
+      <div>
+        <button onClick={() => navigate("/login")}>跳转到Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
