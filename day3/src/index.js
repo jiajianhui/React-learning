7540745cc862bda9e7b9a9042c70b15ab5ed4cb3
jiajianhui <jia15176168273@icloud.com>
2024-10-08 10:59:28 +0800

    优化day3文件夹

--- day3/src/index.js
+++ day3/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 // 为react注入store——react-redux负责把redux和react连接起来，内置Provider组件，通过store参数把创建好的store实例注入到应用中，建立连接
 import store from './store';
@@ -20,4 +18,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
