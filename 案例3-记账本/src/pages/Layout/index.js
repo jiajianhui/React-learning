0e722c13af86f223dc6a3693c7c5c54680411029
jiajianhui <jia15176168273@icloud.com>
2024-10-10 10:52:37 +0800

    案例3-记账本；TabBar样式及功能实现

--- 案例3-记账本/src/pages/Layout/index.js
+++ 案例3-记账本/src/pages/Layout/index.js
@@ -1,10 +1,20 @@
 import {Outlet} from 'react-router-dom'
 import {useEffect} from 'react'
 import { useDispatch } from 'react-redux'
+import {useNavigate} from 'react-router-dom'
 // 导入异步获取数据的方法
 import { getBillList } from '../../store/modules/billStore'
 // 导入组件
-import {Button} from 'antd-mobile'
+import {Button, TabBar} from 'antd-mobile'
+// 导入css
+import './index.scss'
+
+// 导入图标
+import {
+    AddCircleOutline,
+    CalculatorOutline,
+    BillOutline
+} from 'antd-mobile-icons'
 
 const Layout = () => {
     const dispatch = useDispatch()
@@ -12,14 +22,50 @@ const Layout = () => {
     useEffect(() => {
         dispatch(getBillList());
     }, [dispatch])
+
+    // 定义tabs
+    const tabs = [
+      {
+        key: "",
+        title: "月度账单",
+        icon: <BillOutline />,
+      },
+      {
+        key: "new",
+        title: "记账",
+        icon: <AddCircleOutline />,
+      },
+      {
+        key: "year",
+        title: "年度账单",
+        icon: <CalculatorOutline />,
+      },
+    ];
+    // tab路由切换函数
+    const navigate = useNavigate();
+    // onChange回调——切换标签时触发该函数，并传递一个参数，该参数默认为标签中的key
+    const switchRoute = (path) => {
+        navigate(path)
+        console.log(path);
+    }
+
     return(
-        <div>
+        <div className='layout'>
 
-            {/* 二级路由 */}
-            <Outlet />
+            <div className='container'>
+                {/* 二级路由 */}
+                <Outlet />
+            </div>
 
-            Layout
-            <Button color='primary'>按钮</Button>
+            {/* tabbar */}
+            <div className='footer'>
+                <TabBar onChange={switchRoute}>
+                {tabs.map(item => (
+                    <TabBar.Item key={item.key} icon={item.icon} title={item.title}>
+                    </TabBar.Item>
+                ))}
+            </TabBar>
+            </div>
         </div>
     )
 }
