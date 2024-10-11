33e3402fffc883976520f499ffb25f9f4f41ed8e
jiajianhui <jia15176168273@icloud.com>
2024-10-11 10:48:21 +0800

    案例3-记账本；统计区域-点击切换时间选择框

--- 案例3-记账本/src/pages/Month/index.js
+++ 案例3-记账本/src/pages/Month/index.js
@@ -1,19 +1,32 @@
 // 导入组件
-import { NavBar } from "antd-mobile";
+import { NavBar, DatePicker } from "antd-mobile";
 
 // 导入样式
 import './index.scss'
 
+import { useState } from "react";
+import classnames from 'classnames'
+
 const Month = () => {
+
+  // 切换时间选择框
+  // 1、使用useState控制显示和隐藏
+  // 2、配置确认、取消相关操作
+  // 3、点击时间区域，控制箭头动画
+  const [dateVisible, setDateVisible] = useState(false)
+  // 确认函数
+  const onConfirm = () => {
+    setDateVisible(false)
+  }
   return (
     <div className="monthBox">
       <NavBar back={null}>月度账单</NavBar>
 
       <div className="header">
         {/* 时间切换区域 */}
-        <div className="date">
+        <div className="date" onClick={() => setDateVisible(true)}>
           <span className="text">2024 | 10月账单</span>
-          <span className="arrow expand"></span>
+          <span className={classnames('arrow', dateVisible && 'expand')}></span>
         </div>
 
         {/* 统计区域 */}
@@ -32,6 +45,14 @@ const Month = () => {
           </div>
         </div>
       </div>
+
+      {/* 时间选择器组件 */}
+      <DatePicker
+        title="时间选择"
+        visible={dateVisible}
+        onCancel={() => setDateVisible(false)}
+        onConfirm={onConfirm}
+      />
     </div>
   );
 };
